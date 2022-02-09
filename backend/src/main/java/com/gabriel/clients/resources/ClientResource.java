636c40b1b9d8fef414e0a805893e4adc33677e5a
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-02-09 18:08:02 -0300

    created insert endpoint and fixed mappings (forgot,oops)

--- backend/src/main/java/com/gabriel/clients/resources/ClientResource.java
+++ backend/src/main/java/com/gabriel/clients/resources/ClientResource.java
@@ -1,14 +1,20 @@
 package com.gabriel.clients.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabriel.clients.dtos.ClientDto;
 import com.gabriel.clients.services.ClientService;
@@ -18,23 +24,33 @@ import com.gabriel.clients.services.ClientService;
 public class ClientResource {
 	
 	@Autowired
-	public ClientService service;
+	public ClientService clientService;
 
+	@GetMapping
 	public ResponseEntity<Page<ClientDto>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "linesPerPage", defaultValue = "6") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
 			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
 			) {
 		PageRequest request = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
-		Page<ClientDto> list = service.findAllPaged(request);
+		Page<ClientDto> list = clientService.findAllPaged(request);
 		
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<ClientDto> findByid(@PathVariable(value = "id") Long id) {
-		ClientDto dto = service.findById(id);
+		ClientDto dto = clientService.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDto> insert(@RequestBody ClientDto dto) {
+		dto = clientService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- backend/src/main/java/com/gabriel/clients/services/ClientService.java
+++ backend/src/main/java/com/gabriel/clients/services/ClientService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gabriel.clients.dtos.ClientDto;
@@ -12,11 +13,13 @@ import com.gabriel.clients.entities.Client;
 import com.gabriel.clients.repositories.ClientRepository;
 import com.gabriel.clients.services.exceptions.ResourceNotFoundException;
 
+@Service
 public class ClientService {
 
 	@Autowired
 	private ClientRepository clientRepository;
 
+	@Transactional(readOnly = true)
 	public Page<ClientDto> findAllPaged(PageRequest request) {
 		Page<Client> list = clientRepository.findAll(request);
 		return list.map(item -> new ClientDto(item));
@@ -28,5 +31,21 @@ public class ClientService {
 		Client client = clientOptional.orElseThrow(
 				() -> new ResourceNotFoundException(String.format("Sorry, entity of id %s was not found.", id)));
 		return new ClientDto(client);
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDto insert(ClientDto dto) {
+		Client entity = new Client();
+		toDto(dto, entity);
+		entity = clientRepository.save(entity);
+		return new ClientDto(entity);
+	}
+
+	private void toDto(ClientDto dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
 	}
 }
