61ba99567d5f2949314371dc957ae08a59ab38fc
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-02-09 17:39:55 -0300

    created custom exception handler and exception to treat not found resources

--- backend/src/main/java/com/gabriel/clients/services/ClientService.java
+++ backend/src/main/java/com/gabriel/clients/services/ClientService.java
@@ -1,15 +1,19 @@
 package com.gabriel.clients.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gabriel.clients.dtos.ClientDto;
 import com.gabriel.clients.entities.Client;
 import com.gabriel.clients.repositories.ClientRepository;
+import com.gabriel.clients.services.exceptions.ResourceNotFoundException;
 
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository clientRepository;
 
@@ -17,8 +21,12 @@ public class ClientService {
 		Page<Client> list = clientRepository.findAll(request);
 		return list.map(item -> new ClientDto(item));
 	}
-	
+
+	@Transactional(readOnly = true)
 	public ClientDto findById(Long id) {
-		return null;
+		Optional<Client> clientOptional = clientRepository.findById(id);
+		Client client = clientOptional.orElseThrow(
+				() -> new ResourceNotFoundException(String.format("Sorry, entity of id %s was not found.", id)));
+		return new ClientDto(client);
 	}
 }
---
+++ backend/src/main/java/com/gabriel/clients/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.gabriel.clients.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}
